8abc8076f0f70b00df030509ecf48df9f59c5f54
Lauren Zou <zou.lauren@gmail.com>
2020-05-05 15:06:54 -0400

    Include built files.

---
+++ build/magiclauren.js
@@ -0,0 +1,37 @@
+map_key('W', 'move_up');
+map_key('S', 'move_down');
+map_key('A', 'move_left');
+map_key('D', 'move_right');
+setInterval(function () {
+    use_hp_or_mp();
+    loot();
+    if (is_moving(character))
+        return;
+    var target = get_target();
+    if (!target)
+        target = get_nearest_monster({ max_att: 120, path_check: true });
+    if (!target) {
+        game_log('No monsters here!');
+        return;
+    }
+    change_target(target);
+    if (!is_in_range(target)) {
+        move(character.x + (target.x - character.x) / 3, character.y + (target.y - character.y) / 3);
+    }
+    moveWithinRange(character, target);
+    if (can_attack(target))
+        attack(target);
+    moveOutOfRange(character, target);
+}, 1000 / 4);
+function moveWithinRange(character, target) {
+    var distance = Math.sqrt(Math.pow(character.x - target.x, 2) + Math.pow(character.y - target.y, 2));
+    if (distance < character.range)
+        return;
+    move(character.x + (target.x - character.x) / 4, character.y + (target.y - character.y) / 4);
+}
+function moveOutOfRange(character, target) {
+    var distance = Math.sqrt(Math.pow(character.x - target.x, 2) + Math.pow(character.y - target.y, 2));
+    if (distance > target.range + (character.range - target.range) / 2)
+        return;
+    move(character.x - (target.x - character.x) / 4, character.y - (target.y - character.y) / 4);
+}
